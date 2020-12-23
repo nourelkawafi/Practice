9b91670072f79cf364f7ceed73338f99a42680f3
nourelkawafi <74060642+nourelkawafi@users.noreply.github.com>
2020-12-24 01:42:37 +0200

    Just Practice

--- src/main/java/Nour1.java
+++ src/main/java/Nour1.java
@@ -1,5 +1,12 @@
 public class Nour1 {
     public static void main(String[] args) {
 
+        String star = "********";
+        for (int i = 0; i <star.length()-1 ; i++) {
+            System.out.println(star);
+            //practicing
+
+        }
+
     }
 }
